bba82fdabceecc128ed39ff34b97b9506755953d
Lloyd Wallis <lloyd.wallis@bbc.co.uk>
2016-01-26 12:49:33 +0000

    Keep track of old builds

--- samples/simple-runner.sh
+++ samples/simple-runner.sh
@@ -6,7 +6,17 @@ local_identifier='DASH-IF-dash.js'
 local_path=`pwd`
 browserstack_user=`cat $config_file | jq -r '.secure_configuration["browserstack-user"]'`
 browserstack_key=`cat $config_file | jq -r '.secure_configuration["browserstack-key"]'`
-# Debuggign is useful, but we don't want keys in the logs
+# Running in a CI environment
+if [ ! -z "$BUILD_TAG" ]
+  then
+    local_identifier=$BUILD_TAG
+    # Clean old build artifacts out
+    rm -rf logs/jenkins*
+    mkdir logs
+    # Create/update an index file
+    echo -e "<a href="$BUILD_TAG">$BUILD_TAG</a><br>\n$(head -n 28 logs/index.html)" > logs/index.html
+fi
+# Debugging is useful, but we don't want keys in the logs
 set +x
 
 npm install
